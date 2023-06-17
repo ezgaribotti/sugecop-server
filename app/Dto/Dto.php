459b6dbfbeb4760f0c7d84554fad2ee37f8ac0f5
Ezequiel Garibotti <ezgaribotti@gmail.com>
2023-06-17 18:54:34 -0300

    Dto base creado

---
+++ app/Dto/Dto.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Dto;
+
+abstract class Dto
+{
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+}
