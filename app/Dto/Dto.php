feec51ab27b4b116b345ca6b9c872cad04b55fbd
Ezequiel Garibotti <ezgaribotti@gmail.com>
2023-06-18 17:53:17 -0300

    Ayudante de objeto de transferencia de datos creado

--- app/Dto/Dto.php
+++ app/Dto/Dto.php
@@ -2,10 +2,16 @@
 
 namespace App\Dto;
 
+use App\Entities\Entity;
+
 abstract class Dto
 {
-    public function __construct(array $data = [])
+    public function __construct(array|Entity $data = [])
     {
+        if ($data instanceof Entity) {
+            $data = $data->toArray();
+        }
+
         foreach ($data as $key => $value) {
             if (property_exists($this, $key)) {
                 $this->$key = $value;
